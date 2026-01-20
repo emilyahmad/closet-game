8e50baff7162270da2a3c9f4aa5e6490b11da8fa
Emily <emilyahmad77@gmail.com>
2026-01-20 01:06:30 -0500

    moving bugs fixed

--- player.gd
+++ player.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-var direction: Vector2 = Vector2(1,0)
+var direction: Vector2 = Vector2(1,1)
 var speed: int = 5
 
 func _physics_process(_delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
 	position += direction * speed
