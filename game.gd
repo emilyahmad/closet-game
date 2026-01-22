9ae8774dc600afb6c15cb64c71451de3ae62a5b2
Emily <emilyahmad77@gmail.com>
2026-01-22 01:14:11 -0500

    set up title screen (shows up when hitting a car)

--- game.gd
+++ game.gd
@@ -1,20 +1,26 @@
 extends Node2D
 
 var car_scene: PackedScene = preload("res://car.tscn")
+var score: int
 
-func _on_finish_area_2d_body_entered(body: Node2D) -> void:
-	print(body)
-	print("has entered")
+func _on_finish_area_2d_body_entered(_body: Node2D) -> void:
+	call_deferred("change_scene")
 
+func change_scene():
+	get_tree().change_scene_to_file("res://Title.tscn")
 
 func _on_car_timer_timeout() -> void:
-	#whenever triggered, attach instance of car scene
-	#and add to game
 	var car = car_scene.instantiate() as Area2D
 	$Objects.add_child(car)
 	var pos_marker = $CarStartPositions.get_children().pick_random() as Marker2D
 	car.position = pos_marker.position
 	car.connect("body_entered", go_to_title)
 
-func go_to_title(body):
+func go_to_title(_body):
+	print(_body)
 	print('player car collision')
+	call_deferred("change_scene")
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+	$CanvasLayer/Label.text = str(score)
